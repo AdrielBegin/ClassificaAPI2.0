4a4898b574daf8d53836b6e72209843339d1b642
AdrielBegin <adriel.pemi@gmail.com>
2023-10-17 12:37:25 -0400

    feat: criando migrations

--- Classifica3.0/Migrations/20231017163611_Inicial_correcao.Designer.cs
+++ Classifica3.0/Migrations/20231017163611_Inicial_correcao.Designer.cs
@@ -4,6 +4,7 @@ using Classifica3._0.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Classifica3._0.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017163611_Inicial_correcao")]
+    partial class Inicial_correcao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Classifica3.0/Migrations/20231017163611_Inicial_correcao.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Classifica3._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial_correcao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCardCreated",
+                table: "Cards");
+
+            migrationBuilder.RenameColumn(
+                name: "DataCardUpdated",
+                table: "Cards",
+                newName: "DateCardCreated");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCardUpdated",
+                table: "Cards",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Situacao",
+                table: "Cards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCardUpdated",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "Situacao",
+                table: "Cards");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCardCreated",
+                table: "Cards",
+                newName: "DataCardUpdated");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCardCreated",
+                table: "Cards",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
